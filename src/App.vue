aae0d9f749bac18d2ec07d859350838c2aac9fc5
Juan Pablo Aqueveque <jp.aqueveque@gmail.com>
2024-01-02 11:58:10 -0300

    initial vite setup

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>Bolettta</h1>
+  <h2>Calcula tu boleta de honorarios</h2>
+  <form autocomplete="off">
+    <label for="input">$</label>
+    <input type="text" name="input" />
+  </form>
+  <div class="retention"></div>
+  <div class="result">
+    <div class="net"></div>
+    <div class="gross"></div>
+  </div>
+</template>
+
+<style scoped>
+</style>
