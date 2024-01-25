450951dda421575011ca9d82d47db56cd13915e2
Juan Pablo Aqueveque <jp.aqueveque@gmail.com>
2024-01-25 00:40:26 -0300

    add format to results

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { computed } from 'vue';
+
   const props = defineProps({
     net: Number,
     gross: Number,
@@ -12,13 +14,41 @@
   const resultGross = function(){
     return Math.round( props.state.input * ( 1 - parseFloat( props.state.retention ) / 100) );
   }
+
+	const formattedGross = computed(() => {
+		const grossValue = resultGross();
+    const formattedValue = new Intl.NumberFormat('es-CL', {
+			style: 'currency',
+      currency: 'CLP'
+		}).format(grossValue);
+		return formattedValue;
+	});
+
+	const formattedNet = computed(() => {
+		const netValue = resultNet();
+    const formattedValue = new Intl.NumberFormat('es-CL', {
+			style: 'currency',
+      currency: 'CLP'
+		}).format(netValue);
+		return formattedValue;
+	});
+
+	const formattedMakeNet = computed(() => {
+		const formatted = new Intl.NumberFormat('es-CL', {
+			style: 'currency',
+      currency: 'CLP'
+		}).format(props.state.input);
+		return formatted;
+
+	})
+
 </script>
 
 <template>
   <div>
     <h1>Retencion: {{ parseFloat(props.state.retention) }}%</h1>
-    <div class="net">NET: {{ resultNet() }}</div>
-    <div class="gross">GROSS: {{ resultGross() }}</div>
+    <div class="net">NET: Deberás hacer la boleta por {{ formattedNet }} y recibirás: {{ formattedMakeNet }}</div>
+    <div class="gross">GROSS: Deberás hacer la boleta por {{ formattedMakeNet }} y recibirás {{ formattedGross }}</div>
   </div>
 </template>
 
